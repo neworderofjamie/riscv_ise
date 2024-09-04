6127239818f4814122a89ed6c82549c0dbaeed60
neworderofjamie <neworderofjamie@gmail.com>
2024-09-04 14:46:05 +0100

    handle tests of IMM operations

--- tools/rvtest_code_parser.py
+++ tools/rvtest_code_parser.py
@@ -19,17 +19,15 @@ def _get_assembler_func(op_code):
     return (op_code + "_" if op_code in _underscore_op_codes
             else op_code)
 
+# Useful sub-regular expressions
 def _reg(name):
     return fr"x(?P<{name}>[0-9]+)"
 
 def _num(name):
     return fr"(?P<{name}>-?(?:0x)?[0-9a-fA-F]+)"
 
-# Useful sub-regular expressions
 _inst = r"(?P<inst>[a-z]+)"
 _var = r"([a-zA-Z_][a-zA-Z0-9_]*)"
-
-
 
 # Register-register operation
 # TEST_RR_OP(inst, destreg, reg1, reg2, correctval, val1, val2, swreg, offset, testreg)
@@ -37,7 +35,7 @@ _match_test_rr_op = re.compile(fr"TEST_RR_OP\(\s*{_inst},\s*{_reg('destreg')},\s
 
 # Register-immediate instruction
 # TEST_IMM_OP( inst, destreg, reg, correctval, val, imm, swreg, offset, testreg)	
-#_match_test_imm_op = re.compile(fr"TEST_IMM_OP\(\s*{_inst},\s*{_reg},\s*{_reg},\s*{_num},\s*{_num},\s*{_num},\s*{_reg},\s*{_num},\s*{_reg}\)")
+_match_test_imm_op = re.compile(fr"TEST_IMM_OP\(\s*{_inst},\s*{_reg('destreg')},\s*{_reg('reg')},\s*{_num('correctval')},\s*{_num('val')},\s*{_num('imm')},\s*{_reg('swreg')},\s*{_num('offset')},\s*{_reg('testreg')}\)")
 
 # Base address setting operation
 _match_sigbase = re.compile(fr"RVTEST_SIGBASE\(\s*{_reg('basereg')},\s*{_var}\)")
@@ -93,9 +91,17 @@ def parse_code(lines, var_addresses):
                 correct_outputs.append((result_address, int(match.group("correctval"), base=0),
                                         _get_description(lines, i)))
             # If line contains register-immediate operation
-            #elif (match := _match_test_imm_op.search(l)) is not None:
-            #    # Gen
-            #    print("IMMMM")
+            elif (match := _match_test_imm_op.search(l)) is not None:
+                # Generate code to load operands from immediates, perform operation and store result
+                test_code += (
+                    f"c.li(Reg::X{match.group('reg')}, MASK_XLEN({match.group('val')}));\n"
+                    f"c.{_get_assembler_func(match.group('inst'))}(Reg::X{match.group('destreg')}, Reg::X{match.group('reg')}, SEXT_IMM({match.group('imm')}));\n"
+                    f"c.sw(Reg::X{match.group('destreg')}, Reg::X{match.group('swreg')}, {match.group('offset')});\n\n")
+                
+                # Calculate destination address and add result to check
+                result_address = base_addresses[match.group("swreg")] + int(match.group("offset"), 0)
+                correct_outputs.append((result_address, int(match.group("correctval"), base=0),
+                                        _get_description(lines, i)))
             # If line contains test case definition, skip
             elif l.startswith("RVTEST_CASE"):
                 continue
