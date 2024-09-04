216cb423cc73368d1b1aa5ac84860e94c5c39a78
neworderofjamie <neworderofjamie@gmail.com>
2024-09-04 14:36:05 +0100

    switch to swanky named match groups in regex

--- tools/rvtest_code_parser.py
+++ tools/rvtest_code_parser.py
@@ -5,21 +5,6 @@ from utils import get_clean_name
 
 logger = logging.getLogger(__name__)
 
-# Useful sub-regular expressions
-_inst = r"([a-z]+)"
-_reg = r"x([0-9]+)"
-_num = r"(-?(?:0x)?[0-9a-fA-F]+)"
-_var = r"([a-zA-Z_][a-zA-Z0-9_]*)"
-
-# Register-register operation
-# TEST_RR_OP(inst, destreg, reg1, reg2, correctval, val1, val2, swreg, offset, testreg)
-_match_test_rr_op = re.compile(fr"TEST_RR_OP\({_inst},\s*{_reg},\s*{_reg},\s*{_reg},\s*{_num},\s*{_num},\s*{_num},\s*{_reg},\s*{_num},\s*{_reg}\)")
-
-# Base address setting operation
-_match_sigbase = re.compile(fr"RVTEST_SIGBASE\(\s*{_reg},\s*{_var}\)")
-
-# Set of op-codes whose names are keywords in C++ and thus have underscores
-_underscore_op_codes = set(["xor", "or", "and"])
 
 def _get_description(lines, i):
     # If there is a previous line and it starts with a comment
@@ -33,6 +18,33 @@ def _get_description(lines, i):
 def _get_assembler_func(op_code):
     return (op_code + "_" if op_code in _underscore_op_codes
             else op_code)
+
+def _reg(name):
+    return fr"x(?P<{name}>[0-9]+)"
+
+def _num(name):
+    return fr"(?P<{name}>-?(?:0x)?[0-9a-fA-F]+)"
+
+# Useful sub-regular expressions
+_inst = r"(?P<inst>[a-z]+)"
+_var = r"([a-zA-Z_][a-zA-Z0-9_]*)"
+
+
+
+# Register-register operation
+# TEST_RR_OP(inst, destreg, reg1, reg2, correctval, val1, val2, swreg, offset, testreg)
+_match_test_rr_op = re.compile(fr"TEST_RR_OP\(\s*{_inst},\s*{_reg('destreg')},\s*{_reg('reg1')},\s*{_reg('reg2')},\s*{_num('correctval')},\s*{_num('val1')},\s*{_num('val2')},\s*{_reg('swreg')},\s*{_num('offset')},\s*{_reg('testreg')}\)")
+
+# Register-immediate instruction
+# TEST_IMM_OP( inst, destreg, reg, correctval, val, imm, swreg, offset, testreg)	
+#_match_test_imm_op = re.compile(fr"TEST_IMM_OP\(\s*{_inst},\s*{_reg},\s*{_reg},\s*{_num},\s*{_num},\s*{_num},\s*{_reg},\s*{_num},\s*{_reg}\)")
+
+# Base address setting operation
+_match_sigbase = re.compile(fr"RVTEST_SIGBASE\(\s*{_reg('basereg')},\s*{_var}\)")
+
+# Set of op-codes whose names are keywords in C++ and thus have underscores
+_underscore_op_codes = set(["xor", "or", "and"])
+
 
 def parse_code(lines, var_addresses):
     test_code = ""
@@ -59,7 +71,7 @@ def parse_code(lines, var_addresses):
             # If line contains base address setting operation
             elif (match := _match_sigbase.search(l)) is not None:
                 base_address = var_addresses[match.group(2)]
-                base_reg = match.group(1)
+                base_reg = match.group("basereg")
                 
                 # Add base address to dictionary
                 base_addresses[base_reg] = base_address
@@ -69,17 +81,21 @@ def parse_code(lines, var_addresses):
        
             # If line contains register-register operation
             elif (match := _match_test_rr_op.search(l)) is not None:
-                # Generate code to load immediate values, perform operation and store result
+                # Generate code to load operands from immediates, perform operation and store result
                 test_code += (
-                    f"c.li(Reg::X{match.group(3)}, MASK_XLEN({match.group(6)}));\n"
-                    f"c.li(Reg::X{match.group(4)}, MASK_XLEN({match.group(7)}));\n"
-                    f"c.{_get_assembler_func(match.group(1))}(Reg::X{match.group(2)}, Reg::X{match.group(3)}, Reg::X{match.group(4)});\n"
-                    f"c.sw(Reg::X{match.group(2)}, Reg::X{match.group(8)}, {match.group(9)});\n\n")
+                    f"c.li(Reg::X{match.group('reg1')}, MASK_XLEN({match.group('val1')}));\n"
+                    f"c.li(Reg::X{match.group('reg2')}, MASK_XLEN({match.group('val2')}));\n"
+                    f"c.{_get_assembler_func(match.group('inst'))}(Reg::X{match.group('destreg')}, Reg::X{match.group('reg1')}, Reg::X{match.group('reg2')});\n"
+                    f"c.sw(Reg::X{match.group('destreg')}, Reg::X{match.group('swreg')}, {match.group('offset')});\n\n")
                 
                 # Calculate destination address and add result to check
-                result_address = base_addresses[match.group(8)] + int(match.group(9), 0)
-                correct_outputs.append((result_address, int(match.group(5), base=0),
+                result_address = base_addresses[match.group("swreg")] + int(match.group("offset"), 0)
+                correct_outputs.append((result_address, int(match.group("correctval"), base=0),
                                         _get_description(lines, i)))
+            # If line contains register-immediate operation
+            #elif (match := _match_test_imm_op.search(l)) is not None:
+            #    # Gen
+            #    print("IMMMM")
             # If line contains test case definition, skip
             elif l.startswith("RVTEST_CASE"):
                 continue
