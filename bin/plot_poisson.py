042a3ac4ab419cfca35d607a4cc02bca30f40b7b
neworderofjamie <neworderofjamie@gmail.com>
2024-10-07 09:54:20 +0100

    plotting script for poisson

---
+++ bin/plot_poisson.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from scipy.stats import poisson
+
+poisson_data = np.loadtxt("out_poisson.txt", dtype=int)
+x = np.arange(np.amin(poisson_data), 1 + np.amax(poisson_data))
+
+fig, axis = plt.subplots()
+axis.hist(poisson_data, bins=x, density=True)
+axis.plot(x, poisson.pmf(x, 5.12))
+axis.set_xlabel("k")
+axis.set_ylabel("P(x=k)")
+plt.show()
