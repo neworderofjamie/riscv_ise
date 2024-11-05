4475b36036488e69d03842eb981250c0eecbcafd
neworderofjamie <neworderofjamie@gmail.com>
2024-11-05 16:51:58 +0000

    better alif plotting script

--- bin/plot_alif_test.py
+++ bin/plot_alif_test.py
@@ -1,11 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-d = np.loadtxt("out_aliftest_14_9.txt", delimiter=",")
+alif_sim = np.loadtxt("out_aliftest_14_9.txt", delimiter=",")
+alif_device = np.loadtxt("out_aliftest_14_9.txt", delimiter=",")
 
-v = d[:, 0]
-a = d[:, 1]
-fig, axis = plt.subplots()
-a_axis = axis.twinx()
-axis.plot(v)
-a_axis.plot(a)
+fig, axes = plt.subplots(2, sharex=True)
+axes[0].plot(alif_sim[:, 0], label="Sim")
+axes[0].plot(alif_device[:, 0], label="Device")
+axes[0].legend()
+
+axes[1].plot(alif_sim[:, 1], label="Sim")
+axes[1].plot(alif_device[:, 1], label="Device")
+axes[1].legend()
+
 plt.show()
