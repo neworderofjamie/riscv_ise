373973467b21ffe9a65510bc1880929d8aae6528
neworderofjamie <neworderofjamie@gmail.com>
2025-03-19 11:57:09 +0000

    much simpler memory allocator

--- include/compiler/memory_allocator.h
+++ include/compiler/memory_allocator.h
@@ -21,56 +21,38 @@ class Variable;
 class MemoryAllocator
 {
 public:
-    MemoryAllocator(uint32_t bramSizeBytes = 128 * 1024,
-                    uint32_t uramSizeBytes = 8 * 4096 * 2 * 8)
-    :   m_BRAMSizeBytes(bramSizeBytes), m_URAMSizeBytes(uramSizeBytes),
-        m_BRAMHighWaterBytes(0), m_URAMHighWaterBytes(0)
+    MemoryAllocator(uint32_t sizeBytes, uint32_t alignementBytes)
+    :   m_SizeBytes(sizeBytes), m_AlignementBytes(alignementBytes), m_HighWaterBytes(0)
     {}
+    MemoryAllocator(const MemoryAllocator&) = delete;
 
-    //------------------------------------------------------------------------
-    // Public API
-    //------------------------------------------------------------------------
-    uint32_t allocate(const Variable &variable);
-    uint32_t allocate(const EventContainer &eventContainer);
+    uint32_t allocate(uint32_t sizeBytes);
+    uint32_t allocate(uint32_t elementSize, uint32_t count){ return allocate(elementSize * count); }
 
-    size_t getFreeURAMBytes() const{ return m_URAMSizeBytes - m_URAMHighWaterBytes; }
-    size_t getFreeBRAMBytes() const{ return m_BRAMSizeBytes - m_BRAMHighWaterBytes; };
+    uint32_t getFreeBytes() const{ return m_SizeBytes - m_HighWaterBytes; }
 
 private:
-    //------------------------------------------------------------------------
-    // Members
-    //------------------------------------------------------------------------
-    uint32_t m_BRAMSizeBytes;
-    uint32_t m_URAMSizeBytes;
-    uint32_t m_BRAMHighWaterBytes;
-    uint32_t m_URAMHighWaterBytes;
+    uint32_t m_SizeBytes;
+    uint32_t m_AlignementBytes;
+    uint32_t m_HighWaterBytes;
 };
 
 //----------------------------------------------------------------------------
-// MemoryAllocatorVisitor
+// BRAMAllocator
 //----------------------------------------------------------------------------
-class MemoryAllocatorVisitor : public ModelComponentVisitor
+class BRAMAllocator : public MemoryAllocator
 {
 public:
-    MemoryAllocatorVisitor(MemoryAllocator &memoryAllocator);
+    BRAMAllocator() : MemoryAllocator(128 * 1024, 4)
+    {}
+};
 
-    //------------------------------------------------------------------------
-    // ModelComponentVisitor virtuals
-    //------------------------------------------------------------------------
-    virtual void visit(const EventContainer &eventContainer) final;
-    virtual void visit(const Variable &variable) final;
-
-    //------------------------------------------------------------------------
-    // Public API
-    //------------------------------------------------------------------------
-    const auto &getURAMAllocations() const{ return m_URAMAllocations; }
-    const auto &getBRAMAllocations() const{ return m_BRAMAllocations; }
-
-private:
-    //------------------------------------------------------------------------
-    // Members
-    //------------------------------------------------------------------------
-    std::reference_wrapper<MemoryAllocator> m_MemoryAllocator;
-    std::unordered_map<const ModelComponent*, uint32_t> m_URAMAllocations;
-    std::unordered_map<const ModelComponent*, uint32_t> m_BRAMAllocations;
+//----------------------------------------------------------------------------
+// URAMAllocator
+//----------------------------------------------------------------------------
+class URAMAllocator : public MemoryAllocator
+{
+public:
+    URAMAllocator() : MemoryAllocator(8 * 4096 * 2 * 8, 64)
+    {}
 };
