88da082e89d48a97363604604f6e927a9e1dcac8
neworderofjamie <neworderofjamie@gmail.com>
2025-03-20 16:15:22 +0000

    first pass implementation of FeNN simulator state and arra classes

---
+++ src/backend/backend_fenn_sim.cc
@@ -0,0 +1,182 @@
+#include "backend/backend_fenn_sim.h"
+
+// Standard C++ includes
+#include <stdexcept>
+
+// Plog includes
+#include <plog/Log.h>
+
+// Common includes
+#include "ise/riscv.h"
+#include "ise/vector_processor.h"
+
+// Compiler includes
+#include "compiler/memory_allocator.h"
+
+//------------------------------------------------------------------------
+// Anonymous namespace
+//------------------------------------------------------------------------
+namespace
+{
+class State : public StateBase
+{
+public:
+    //------------------------------------------------------------------------
+    // Public API
+    //------------------------------------------------------------------------
+    const auto &getRISCV() const{ return m_RISCV; }
+    auto &getRISCV(){ return m_RISCV; }
+
+    const auto &getBRAMAllocator() const{ return m_BRAMAllocator; }
+    auto &getBRAMAllocator(){ return m_BRAMAllocator; }
+
+    const auto &getURAMAllocator() const{ return m_URAMAllocator; }
+    auto &getURAMAllocator(){ return m_URAMAllocator; }
+
+private:
+    //------------------------------------------------------------------------
+    // Members
+    //------------------------------------------------------------------------
+    RISCV m_RISCV;
+    BRAMAllocator m_BRAMAllocator;
+    URAMAllocator m_URAMAllocator;
+};
+
+
+//------------------------------------------------------------------------
+// URAMArray
+//------------------------------------------------------------------------
+//! Class for managing arrays in URAM. Host memory lives in 
+//! (uncached) DMA buffer and is transferred to FeNN using DMA controller
+class URAMArray : public URAMArrayBase
+{
+public:
+     URAMArray(const GeNN::Type::ResolvedType &type, size_t count, State &state)
+    :   URAMArrayBase(type, count), m_State(state)
+    {
+        // Allocate if count is specified
+        if(count > 0) {
+            allocate(count);
+        }
+    }
+
+    //------------------------------------------------------------------------
+    // ArrayBase virtuals
+    //------------------------------------------------------------------------
+    //! Allocate array
+    virtual void allocate(size_t count) final override
+    {
+        // Set count
+        setCount(count);
+
+        // Allocate memory for host pointer
+        setHostPointer(new uint8_t[getSizeBytes()]);
+
+        // Allocate URAM
+        setURAMPointer(m_State.get().getURAMAllocator().allocate(getSizeBytes()));
+    }
+
+    //! Free array
+    virtual void free() final override
+    {
+        delete [] getHostPointer();
+        setHostPointer(nullptr);
+        setURAMPointer(std::nullopt);
+        setCount(0);
+    }
+
+    //! Copy entire array to device
+    virtual void pushToDevice() final override
+    {
+        // Copy correct number of int16_t from host pointer to vector data memory
+        auto &vectorDataMemory = m_State.get().getRISCV().getCoprocessor<VectorProcessor>(vectorQuadrant)->getVectorDataMemory();
+        std::copy_n(getHostPointer<int16_t>(), getCount(), 
+                    vectorDataMemory.getData() + (getURAMPointer() / 2));
+    }
+
+    //! Copy entire array from device
+    virtual void pullFromDevice() final override
+    {
+        LOGW << "Copying URAM buffers is implemented in simulation for convenience but currently doens't work on device";
+        
+        // Copy correct number of int16_t from vector data memory to host pointer
+        const auto &vectorDataMemory = m_State.get().getRISCV().getCoprocessor<VectorProcessor>(vectorQuadrant)->getVectorDataMemory();
+        std::copy_n(vectorDataMemory.getData() + (getURAMPointer() / 2), getCount(), 
+                    getHostPointer<int16_t>());
+    }
+
+private:
+    std::reference_wrapper<State> m_State;
+};
+
+//------------------------------------------------------------------------
+// BRAMArray
+//------------------------------------------------------------------------
+//! Class for managing arrays in BRAM. Host memory is allocated using standard
+//! allocator and is transferred to FeNN using Device functionality
+class BRAMArray : public BRAMArrayBase
+{
+public:
+     BRAMArray(const GeNN::Type::ResolvedType &type, size_t count, State &state)
+    :   BRAMArrayBase(type, count), m_State(state)
+    {
+        // Allocate if count is specified
+        if(count > 0) {
+            allocate(count);
+        }
+    }
+
+    virtual ~BRAMArray()
+    {
+        if(getCount() > 0) {
+            free();
+        }
+    }
+
+    //------------------------------------------------------------------------
+    // ArrayBase virtuals
+    //------------------------------------------------------------------------
+    //! Allocate array
+    virtual void allocate(size_t count) final override
+    {
+         // Set count
+        setCount(count);
+
+        // Allocate memory for host pointer
+        setHostPointer(new uint8_t[getSizeBytes()]);
+
+        // Allocate BRAM
+        setBRAMPointer(m_State.get().getBRAMAllocator().allocate(getSizeBytes()));
+    }
+
+    //! Free array
+    virtual void free() final override
+    {
+        delete [] getHostPointer();
+        setHostPointer(nullptr);
+        setBRAMPointer(std::nullopt);
+        setCount(0);
+    }
+
+    //! Copy entire array to device
+    virtual void pushToDevice() final override
+    {
+        // Copy correct number of int16_t from host pointer to vector data memory
+        auto &scalarDataMemory = m_State.get().getRISCV().getScalarDataMemory();
+        std::copy_n(getHostPointer<uint8_t>(), getSizeBytes(), 
+                    scalarDataMemory.getData() + getBRAMPointer());
+    }
+
+    //! Copy entire array from device
+    virtual void pullFromDevice() final override
+    {
+        // Copy correct number of int16_t from vector data memory to host pointer
+        const auto &scalarDataMemory = m_State.get().getRISCV().getScalarDataMemory();
+        std::copy_n(scalarDataMemory.getData() + getBRAMPointer(), getSizeBytes(), 
+                    getHostPointer<uint8_t>());
+    }
+
+private:
+     std::reference_wrapper<State> m_State;
+};
+}
