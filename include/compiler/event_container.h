e68c391239439dda9bc28b430234724fdef815df
neworderofjamie <neworderofjamie@gmail.com>
2025-03-24 17:31:16 +0000

    * Allow event containers to buffer events for multiple timesteps  - will enable both delays and direct input spike injection * Check event containers with buffering aren't being used as neuron update process outputs * Calculate index into input events code when generating event propagation process code

--- include/compiler/event_container.h
+++ include/compiler/event_container.h
@@ -14,21 +14,24 @@
 class EventContainer : public AcceptableModelComponent<EventContainer, State>
 {
 public:
-    EventContainer(Private, const Shape &shape, const std::string &name)
-    :   AcceptableModelComponent<EventContainer, State>(name), m_Shape(shape)
+    EventContainer(Private, const Shape &shape, size_t numBufferTimesteps, const std::string &name)
+    :   AcceptableModelComponent<EventContainer, State>(name), m_Shape(shape),
+        m_NumBufferTimesteps(numBufferTimesteps)
     {}
 
     //------------------------------------------------------------------------
     // Public API
     //------------------------------------------------------------------------
     const auto &getShape() const{ return m_Shape; }
+    size_t getNumBufferTimesteps() const { return m_NumBufferTimesteps; }
 
     //------------------------------------------------------------------------
     // Static API
     //------------------------------------------------------------------------
-    static std::shared_ptr<EventContainer> create(const Shape &shape, const std::string &name = "")
+    static std::shared_ptr<EventContainer> create(const Shape &shape, size_t numBufferTimesteps = 1,
+                                                  const std::string &name = "")
     {
-        return std::make_shared<EventContainer>(Private(), shape, name);
+        return std::make_shared<EventContainer>(Private(), shape, numBufferTimesteps, name);
     }
 
 private:
@@ -36,4 +39,5 @@ private:
     // Members
     //------------------------------------------------------------------------
     Shape m_Shape;
+    size_t m_NumBufferTimesteps;
 };
