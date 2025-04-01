c0597ec461a800c5f266a511ee28cb168d695886
neworderofjamie <neworderofjamie@gmail.com>
2025-04-01 11:55:36 +0100

    add context string to ``MemoryAllocator`` to improve debug output

--- src/backend/memory_allocator.cc
+++ src/backend/memory_allocator.cc
@@ -26,7 +26,7 @@ size_t MemoryAllocator::allocate(size_t sizeBytes)
         throw std::runtime_error("Memory exceeded");
     }
 
-    LOGD << "Allocating " << varSize << " bytes of memory starting at " << m_HighWaterBytes << " bytes";    
+    LOGD << "Allocating " << varSize << " bytes of " << m_Context << " starting at " << m_HighWaterBytes << " bytes";    
 
     // Stash old high-water mark, update and return
     const size_t address = m_HighWaterBytes;
@@ -39,6 +39,6 @@ size_t MemoryAllocator::allocate(size_t sizeBytes)
 //----------------------------------------------------------------------------
 // **THINK** do we want to vector align DMA buffer and remove hardware alignement
 DMABufferAllocator::DMABufferAllocator(const DMABuffer &dmaBuffer)
-:   MemoryAllocator(dmaBuffer.getSize(), 4)
+:   MemoryAllocator(dmaBuffer.getSize(), 4, "DMA buffer")
 {
 }
