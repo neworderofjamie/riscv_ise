03ba3b277e2d2c92ed2331d274bfb903b4226fca
neworderofjamie <neworderofjamie@gmail.com>
2025-04-11 12:36:36 +0100

    basically PyTorch...

--- examples/mnist_classifier.py
+++ examples/mnist_classifier.py
@@ -38,13 +38,72 @@ def zero_and_push(state, runtime: Runtime):
     # Push to device
     array.push_to_device()
 
+class LIF:
+    def __init__(self, shape, tau_m, tau_refrac, v_thresh,
+                 record_spikes = False, dtype = "s10_5_sat_t"):
+        self.shape = Shape(shape)
+        dtype = UnresolvedType(dtype)
+        self.v = Variable(self.shape, dtype)
+        self.i = Variable(self.shape, dtype)
+        self.refrac_time = Variable(self.shape, UnresolvedType("int16_t"))
+        self.out_spikes = EventContainer(self.shape, 
+                                         (num_timesteps if record_spikes
+                                          else 1))
+        self.process = NeuronUpdateProcess(
+            """
+            V = (Alpha * V) + I;
+            I = 0.0h5;
+            if (RefracTime > 0) {
+               RefracTime -= 1;
+            }
+            else if(V >= VThresh) {
+               Spike();
+               V -= VThresh;
+               RefracTime = TauRefrac;
+            }
+            """,
+            {"Alpha": Parameter(NumericValue(np.exp(-1.0 / tau_m)), dtype),
+             "VThresh": Parameter(NumericValue(v_thresh), dtype),
+             "TauRefrac": Parameter(NumericValue(tau_refrac), UnresolvedType("int16_t"))},
+            {"V": self.v, "I": self.i, "RefracTime": self.refrac_time},
+            {"Spike": self.out_spikes})
+
+class LI:
+    def __init__(self, shape, tau_m, num_timesteps, dtype = "s10_5_sat_t"):
+        self.shape = Shape(shape)
+        dtype = UnresolvedType(dtype)
+
+        self.v = Variable(self.shape, dtype)
+        self.i = Variable(self.shape, dtype)
+        self.v_avg = Variable(self.shape, dtype)
+        self.bias = Variable(self.shape, dtype)
+        self.process = NeuronUpdateProcess(
+            """
+            V = (Alpha * V) + I + Bias;
+            I = 0.0h6;
+            VAvg += (VAvgScale * V);
+            """,
+            {"Alpha": Parameter(NumericValue(np.exp(-1.0 / tau_m)), dtype), 
+             "VAvgScale": Parameter(NumericValue(1.0 / (num_timesteps / 2)), dtype)},
+            {"V": self.v, "VAvg": self.v_avg, "I": self.i, "Bias": self.bias})
+
+class Linear:
+    def __init__(self, source_events, target_var, weight_dtype):
+        self.shape = Shape([source_events.shape.num_neurons,
+                            target_var.shape.num_neurons])
+        weight_dtype = UnresolvedType(weight_dtype)
+
+        self.weight = Variable(self.shape, weight_dtype)
+        self.process = EventPropagationProcess(source_events, self.weight,
+                                               target_var)
+
 num_examples = 10000
 num_timesteps = 79
-input_shape = Shape([28 * 28])
-hidden_shape = Shape([128])
-output_shape = Shape([10])
-input_hidden_shape = Shape([28 * 28, 128])
-hidden_output_shape = Shape([128, 10])
+input_shape = [28 * 28]
+hidden_shape = [128]
+output_shape = [10]
+input_hidden_shape = [28 * 28, 128]
+hidden_output_shape = [128, 10]
 device = False
 record = False
 
@@ -53,58 +112,17 @@ num_input_spike_words = ceil_divide(28 * 28, 32)
 num_input_spike_array_words = num_input_spike_words * num_timesteps
     
 # Input spikes
-input_spikes = EventContainer(input_shape, num_timesteps)
+input_spikes = EventContainer(Shape(input_shape), num_timesteps)
 
-# Hidden neurons
-hidden_v = Variable(hidden_shape, UnresolvedType("s10_5_sat_t"))
-hidden_i = Variable(hidden_shape, UnresolvedType("s10_5_sat_t"))
-hidden_refrac_time = Variable(hidden_shape, UnresolvedType("int16_t"))
-hidden_spikes = EventContainer(hidden_shape, num_timesteps if record else 1)
-hidden = NeuronUpdateProcess(
-    """
-    V = (Alpha * V) + I;
-    I = 0.0h5;
-    if (RefracTime > 0) {
-       RefracTime -= 1;
-    }
-    else if(V >= VThresh) {
-       Spike();
-       V -= VThresh;
-       RefracTime = TauRefrac;
-    }
-    """,
-    {"Alpha": Parameter(NumericValue(np.exp(-1.0 / 20.0)), UnresolvedType("s10_5_sat_t")),
-     "VThresh": Parameter(NumericValue(0.61), UnresolvedType("s10_5_sat_t")),
-     "TauRefrac": Parameter(NumericValue(5), UnresolvedType("int16_t"))},
-    {"V": hidden_v, "I": hidden_i, "RefracTime": hidden_refrac_time},
-    {"Spike": hidden_spikes})
-
-# Output neurons
-output_v = Variable(output_shape, UnresolvedType("s9_6_sat_t"))
-output_i = Variable(output_shape, UnresolvedType("s9_6_sat_t"))
-output_v_avg = Variable(output_shape, UnresolvedType("s9_6_sat_t"))
-output_bias = Variable(output_shape, UnresolvedType("s9_6_sat_t"))
-output = NeuronUpdateProcess(
-    """
-    V = (Alpha * V) + I + Bias;
-    I = 0.0h6;
-    VAvg += (VAvgScale * V);
-    """,
-    {"Alpha": Parameter(NumericValue(np.exp(-1.0 / 20.0)), UnresolvedType("s9_6_sat_t")), 
-     "VAvgScale": Parameter(NumericValue(1.0 / (num_timesteps / 2)), UnresolvedType("s9_6_sat_t"))},
-    {"V": output_v, "VAvg": output_v_avg, "I": output_i, "Bias": output_bias})
-
-# Input->Hidden event propagation
-input_hidden_weight = Variable(input_hidden_shape, UnresolvedType("s10_5_sat_t"))
-input_hidden = EventPropagationProcess(input_spikes, input_hidden_weight, hidden_i)
-
-# Hidden->Output event propagation
-hidden_output_weight = Variable(hidden_output_shape, UnresolvedType("s9_6_sat_t"))
-hidden_output = EventPropagationProcess(hidden_spikes, hidden_output_weight, output_i)
+# Model
+hidden = LIF(hidden_shape, 20.0, 5, 0.61, record, "s10_5_sat_t")
+output = LI(output_shape, 20.0, num_timesteps, "s9_6_sat_t")
+input_hidden = Linear(input_spikes, hidden.i, "s10_5_sat_t")
+hidden_output = Linear(hidden.out_spikes, output.i, "s9_6_sat_t")
 
 # Group processes
-neuron_update_processes = ProcessGroup([hidden, output])
-synapse_update_processes = ProcessGroup([input_hidden, hidden_output])
+neuron_update_processes = ProcessGroup([hidden.process, output.process])
+synapse_update_processes = ProcessGroup([input_hidden.process, hidden_output.process])
 
 # Create model
 model = Model([neuron_update_processes, synapse_update_processes])
@@ -127,17 +145,17 @@ runtime.set_instructions(code)
 runtime.allocate()
 
 # Load weights
-load_and_push("mnist_in_hid.bin", input_hidden_weight, runtime)
-load_and_push("mnist_hid_out.bin", hidden_output_weight, runtime)
-load_and_push("mnist_bias.bin", output_bias, runtime)
+load_and_push("mnist_in_hid.bin", input_hidden.weight, runtime)
+load_and_push("mnist_hid_out.bin", hidden_output.weight, runtime)
+load_and_push("mnist_bias.bin", output.bias, runtime)
 
 # Zero remaining state
-zero_and_push(hidden_v, runtime)
-zero_and_push(hidden_i, runtime)
-zero_and_push(hidden_refrac_time, runtime)
-zero_and_push(output_v, runtime)
-zero_and_push(output_i, runtime)
-zero_and_push(output_v_avg, runtime)
+zero_and_push(hidden.v, runtime)
+zero_and_push(hidden.i, runtime)
+zero_and_push(hidden.refrac_time, runtime)
+zero_and_push(output.v, runtime)
+zero_and_push(output.i, runtime)
+zero_and_push(output.v_avg, runtime)
 
 # Load data
 mnist_spikes = np.fromfile("mnist_spikes.bin", dtype=np.uint32)
@@ -147,12 +165,13 @@ mnist_labels = np.fromfile("mnist_labels.bin", dtype=np.int16)
 input_spike_array = runtime.get_array(input_spikes)
 input_spike_view = np.asarray(input_spike_array.host_view).view(np.uint32)
 assert not input_spike_view.flags["OWNDATA"]
-hidden_spike_array = runtime.get_array(hidden_spikes)
-output_v_avg_array = runtime.get_array(output_v_avg)
+hidden_spike_array = runtime.get_array(hidden.out_spikes)
+output_v_avg_array = runtime.get_array(output.v_avg)
 output_v_avg_view = np.asarray(output_v_avg_array.host_view).view(np.int16)
 assert not output_v_avg_view.flags["OWNDATA"]
 num_correct = 0
 for i in range(num_examples):
+    print(i)
     # Copy data to array host pointe
     start_word = i * num_input_spike_array_words
     input_spike_view[:] = mnist_spikes[start_word: start_word + num_input_spike_array_words]
