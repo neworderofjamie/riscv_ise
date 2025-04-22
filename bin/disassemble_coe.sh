65396bbda8b23780697393311ad0c7b5e334cca9
neworderofjamie <neworderofjamie@gmail.com>
2025-04-22 16:12:49 +0100

    disassembler build

--- bin/disassemble_coe.sh
+++ bin/disassemble_coe.sh
@@ -4,6 +4,6 @@ tail -n +3 $1 | while read LINE
 do
     LINE_NO_COMMA="${LINE%,}"
     LINE_NO_SUFFIX="${LINE_NO_COMMA%;}"
-    ./disassembler 0x$LINE_NO_SUFFIX
+    ./disassembler_cli 0x$LINE_NO_SUFFIX
    # do something with $line here
 done
