4bcc57d8448841bfaffab704256794242e874749
neworderofjamie <neworderofjamie@gmail.com>
2025-04-22 16:31:37 +0100

    expose disassembler to Python

--- pyfenn/__init__.py
+++ pyfenn/__init__.py
@@ -5,12 +5,12 @@ import sys
 from ._fenn import (BackendFeNNSim, EventContainer, EventPropagationProcess,
                     Model, ModelComponent, NeuronUpdateProcess, NumericValue,
                     Parameter, Process, ProcessGroup, Runtime, Shape, State,
-                    UnresolvedType, Variable)
+                    UnresolvedType, Variable, disassemble)
 
 __all__ = ["BackendFeNNSim", "EventContainer", "EventPropagationProcess",
            "Model", "ModelComponent", "NeuronUpdateProcess", "NumericValue",
            "Parameter", "Process", "ProcessGroup", "Runtime", "Shape", "State",
-           "UnresolvedType", "Variable"]
+           "UnresolvedType", "Variable", disassemble]
 
 if sys.version_info >= (3, 8):
     from importlib import metadata
