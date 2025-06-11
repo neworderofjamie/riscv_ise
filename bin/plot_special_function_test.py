6fd1c9919700bfba0890868a4e7beed63e5e8b20
neworderofjamie <neworderofjamie@gmail.com>
2025-06-11 20:42:34 +0100

    updated plotting script

--- bin/plot_special_function_test.py
+++ bin/plot_special_function_test.py
@@ -3,14 +3,11 @@ import matplotlib.pyplot as plt
 
 faithful_data = np.loadtxt("special_function_output.csv")
 
-log_2 = np.log(2)
-exp_max = 0.5 * log_2
-
-exp_x = np.arange(-exp_max, exp_max, (2 * exp_max) /  (32 * 1024))
-exp_y_correct = np.rint(np.exp(exp_x) * 2**14)
+exp_x = np.linspace(-3.4, 3.4, 3482)
+exp_y_correct = np.rint(np.exp(exp_x) * 2**9)
 
 fig, axis = plt.subplots()
-axis.plot(exp_x, faithful_data, label="Approx")
-axis.plot(exp_x, exp_y_correct, label="Correct")
+axis.plot(exp_x, faithful_data[:3482], label="Approx")
+axis.plot(exp_x, exp_y_correct[:3482], label="Correct")
 axis.legend()
 plt.show()
