b6b09f3a48cacd06efa470ac0fb911dc07db0dfd
neworderofjamie <neworderofjamie@gmail.com>
2025-08-11 18:28:48 +0100

    updated Python bindings and C++ example

--- pyfenn/src/fenn.cc
+++ pyfenn/src/fenn.cc
@@ -306,7 +306,7 @@ PYBIND11_MODULE(_fenn, m)
     // fenn.Model
     //------------------------------------------------------------------------
     pybind11::class_<Model>(m, "Model")
-        .def(pybind11::init<std::vector<std::shared_ptr<const ProcessGroup>>&>());
+        .def(pybind11::init<std::vector<std::shared_ptr<const ProcessGroup>>&, const BackendFeNN&>());
 
     //------------------------------------------------------------------------
     // fenn.BackendFeNN
@@ -319,13 +319,14 @@ PYBIND11_MODULE(_fenn, m)
     // fenn.BackendFeNNSim
     //------------------------------------------------------------------------
     pybind11::class_<BackendFeNNSim, BackendFeNN>(m, "BackendFeNNSim")
-        .def(pybind11::init<>());
+        .def(pybind11::init<bool, size_t>(),
+             pybind11::arg("use_dram_for_weights") = false, pybind11::arg("dma_buffer_size") = 512 * 1024);
 
     //------------------------------------------------------------------------
     // fenn.BackendFeNNHW
     //------------------------------------------------------------------------
     pybind11::class_<BackendFeNNHW, BackendFeNN>(m, "BackendFeNNHW")
-        .def(pybind11::init<>());
+        .def(pybind11::init<bool>(), pybind11::arg("use_dram_for_weights") = false);
 
     //------------------------------------------------------------------------
     // fenn.ArrayBase
