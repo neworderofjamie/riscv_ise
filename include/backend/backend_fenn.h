14c6666b03902c0a77dd9a0a4bda105873b872a0
neworderofjamie <neworderofjamie@gmail.com>
2025-08-14 16:05:43 +0100

    no idea why ``ArrayBase`` and ``IFieldArray`` were in FeNN backend not runtime

--- include/backend/backend_fenn.h
+++ include/backend/backend_fenn.h
@@ -20,96 +20,10 @@
 #include "backend/memory_allocator.h"
 #include "backend/memory_allocator.h"
 #include "backend/model.h"
+#include "backend/runtime.h"
 
 // Forward declarations
 class ProcessGroup;
-
-
-//----------------------------------------------------------------------------
-// ArrayBase
-//----------------------------------------------------------------------------
-//! Base class for arrays created by backend
-//! **NOTE** this is a temporary, simplified version of GeNN's ArrayBase
-class BACKEND_EXPORT ArrayBase
-{
-public:
-    virtual ~ArrayBase() = default;
-
-    //------------------------------------------------------------------------
-    // Declared virtuals
-    //------------------------------------------------------------------------
-    //! Allocate array
-    virtual void allocate(size_t count) = 0;
-
-    //! Free array
-    virtual void free() = 0;
-
-    //! Copy entire array to device
-    virtual void pushToDevice() = 0;
-
-    //! Copy entire array from device
-    virtual void pullFromDevice() = 0;
-
-    //! Serialise backend-specific device object to uint32_t
-    virtual void serialiseDeviceObject(std::vector<std::byte> &bytes) const = 0;
-
-    //! Memset the host pointer
-    virtual void memsetHostPointer(int value);
-
-    //------------------------------------------------------------------------
-    // Public API
-    //------------------------------------------------------------------------
-    const GeNN::Type::ResolvedType &getType() const{ return m_Type; }
-    size_t getCount() const{ return m_Count; };
-    size_t getSizeBytes() const{ return m_Count * m_Type.getValue().size; };
-
-    //! Get array host pointer
-    uint8_t *getHostPointer() const{ return m_HostPointer; }
-
-    template<typename T>
-    T *getHostPointer() const{ return reinterpret_cast<T*>(m_HostPointer); }
-
-protected:
-    ArrayBase(const GeNN::Type::ResolvedType &type, size_t count)
-    :   m_Type(type), m_Count(count), m_HostPointer(nullptr)
-    {
-    }
-
-    //------------------------------------------------------------------------
-    // Protected API
-    //------------------------------------------------------------------------    
-    void setCount(size_t count) { m_Count = count; }
-    void setHostPointer(uint8_t *hostPointer) { m_HostPointer = hostPointer; }
-
-private:
-    //------------------------------------------------------------------------
-    // Members
-    //------------------------------------------------------------------------
-    GeNN::Type::ResolvedType m_Type;
-    size_t m_Count;
-
-    uint8_t *m_HostPointer;
-};
-
-//----------------------------------------------------------------------------
-// IFieldArray
-//----------------------------------------------------------------------------
-//! Interface for object, probably backed by some sort of array,
-//! that anages in-device memory fields structure
-class IFieldArray
-{
-public:
-    virtual ~IFieldArray() = default;
-
-    //------------------------------------------------------------------------
-    // Declared virtuals
-    //------------------------------------------------------------------------
-    //! Sets field at offset to point to array
-    virtual void setFieldArray(uint32_t fieldOffset, const ArrayBase *array) = 0;
-
-    //! Copy field data to device
-    virtual void pushFieldsToDevice() = 0;
-};
 
 //----------------------------------------------------------------------------
 // URAMArrayBase
