a54ce03645d55a02f6f565e7ec6bb7d14ae26697
neworderofjamie <neworderofjamie@gmail.com>
2025-08-22 17:07:17 +0100

    added seperate prefetch and process stages and working on validating traces

--- tools/dma_event_loop_test.cc
+++ tools/dma_event_loop_test.cc
@@ -1,6 +1,8 @@
 // Standard C++ includes
 #include <algorithm>
 #include <iostream>
+#include <numeric>
+#include <unordered_map>
 #include <vector>
 
 // Standard C includes
@@ -19,7 +21,7 @@ auto makeTrace(bool prefetch, uint32_t spikeID, uint32_t row)
     return std::make_tuple(prefetch, spikeID, row);
 }
 
-void generateSpikes(const std::vector<uint32_t> &ids, std::vector<uint32_t> &words)
+void generateSpikeWords(const std::vector<uint32_t> &ids, std::vector<uint32_t> &words)
 {
     for(uint32_t i : ids) {
         assert((i / 32) < words.size());
@@ -32,18 +34,45 @@ void prefetch(uint32_t idPre, uint32_t row, Trace &trace)
     trace.emplace_back(true, idPre, row);
 }
 
-void printTrace(const std::vector<std::tuple<bool, uint32_t>> &trace)
+void process(uint32_t idPre, uint32_t row, Trace &trace)
+{
+    trace.emplace_back(false, idPre, row);
+}
+
+void printTrace(const Trace &trace)
 {
     for(const auto &t : trace) {
-        std::cout << std::get<1>(t) << " - " << std::get<2> << "(" << (std::get<0>(t) ? "prefetch" : "process") << ")" << std::endl;
+        std::cout << std::get<1>(t) << " - " << std::get<2>(t) << "(" << (std::get<0>(t) ? "prefetch" : "process") << ")" << std::endl;
     }
 }
 
-void propagate(const uint32_t *spikeWord, uint32_t numSpikeWords, std::vector<std::tuple<bool, uint32_t>> &trace)
+bool getCounts(const std::vector<uint32_t> &spikeIDs, const Trace &trace, bool prefetchNotProcess)
+{
+    std::unordered_map<uint32_t, uint32_t> spikeIDProcessedCount;
+    std::transform(spikeIDs.cbegin(), spikeIDs.cend(), std::inserter(spikeIDProcessedCount, spikeIDProcessedCount.end()),
+                   [](uint32_t id){ return std::make_pair(id, 0); });
+    
+    for(const auto &t : trace) {
+        // If it's the right sort of trace event
+        if(std::get<0>(t) == prefetchNotProcess) {
+            // Search for spike ID
+            auto idIter = spikeIDProcessedCount.find(std::get<1>(t));
+            assert(idIter != spikeIDProcessedCount.cend());
+            
+            // Increment count
+            idIter->second++;
+        }
+    }
+    
+    return true;
+}
+
+void propagate(const uint32_t *spikeWord, uint32_t numSpikeWords, uint32_t numRows, Trace &trace)
 {
     // Find first spike to prefetch
     uint32_t currentSpikeWord;
     uint32_t idPre;
+    uint32_t prevIdPre;
     uint32_t w = 0;
     for(; w < numSpikeWords; w++) {
         // Get spike word
@@ -60,6 +89,7 @@ void propagate(const uint32_t *spikeWord, uint32_t numSpikeWords, std::vector<st
         else if(currentSpikeWord == 1) {
             // Prefetch first row corresponding to spike
             prefetch(idPre - 31, 0, trace);
+            prevIdPre = idPre - 31;
             
             // Advance to next word and stop searching
             w++;
@@ -79,6 +109,7 @@ void propagate(const uint32_t *spikeWord, uint32_t numSpikeWords, std::vector<st
             idPre -= numLZ;
             
             // Prefetch first row corresponding to spike and stop
+            prevIdPre = idPre;
             prefetch(idPre, 0, trace);
             
             idPre--;
@@ -90,6 +121,15 @@ void propagate(const uint32_t *spikeWord, uint32_t numSpikeWords, std::vector<st
     for(;w < numSpikeWords; w++) {
         // While bits remain
         while(currentSpikeWord != 0) {
+            // Loop through all but last row
+            for(uint32_t r = 0; r < (numRows - 1); r++) {
+                // Prefetch next row
+                prefetch(prevIdPre, r + 1, trace);
+                
+                // Process this row
+                process(prevIdPre, r, trace);
+            }
+    
             // Calculate leading zeros
             const int numLZ = __builtin_clz(currentSpikeWord);
         
@@ -100,12 +140,15 @@ void propagate(const uint32_t *spikeWord, uint32_t numSpikeWords, std::vector<st
             // Subtract number of leading zeros from neuron ID
             idPre -= numLZ;
         
-            // Prefetch spike
-            prefetch(idPre, trace);
+            // Prefetch first row of new spike
+            prefetch(idPre, 0, trace);
             
+            // Process last row of previous spike
+            process(prevIdPre, numRows - 1, trace);
             
-            //process(oldIDPre);
-        
+            // Update previous spike
+            prevIdPre = idPre;
+            
             // New neuron id of the highest bit of this word
             idPre--;
         }
@@ -116,62 +159,80 @@ void propagate(const uint32_t *spikeWord, uint32_t numSpikeWords, std::vector<st
         }
         idPre = ((w + 1) * 32) + 31;
     }
+    
+    // Loop through all but last row
+    for(uint32_t r = 0; r < (numRows - 1); r++) {
+        // Prefetch next row
+        prefetch(prevIdPre, r + 1, trace);
+        
+        // Process this row
+        process(prevIdPre, r, trace);
+    }
+    
+    // Process last row of previous spike
+    process(prevIdPre, numRows - 1, trace);
 }
 }
 
 TEST(DMAEventLoop, NoSpikes)
 {
     std::vector<uint32_t> spikeWords(4, 0);
-    std::vector<std::tuple<bool, uint32_t>> trace;
+    Trace trace;
     
-    propagate(spikeWords.data(), spikeWords.size(), trace);
+    propagate(spikeWords.data(), spikeWords.size(), 1, trace);
     
     ASSERT_TRUE(trace.empty());
 }
 
 TEST(DMAEventLoop, AllSpikes)
 {
-    std::vector<uint32_t> spikeWords(4, 0xFFFFFFFF);
-    std::vector<std::tuple<bool, uint32_t>> trace;
+    std::vector<uint32_t> spikeIDs(4 * 32);
+    std::iota(spikeIDs.begin(), spikeIDs.end(), 0);
     
-    propagate(spikeWords.data(), spikeWords.size(), trace);
+    std::vector<uint32_t> spikeWords(4);
+    generateSpikeWords(spikeIDs, spikeWords);
     
-    std::sort(trace.begin(), trace.end());
-    ASSERT_EQ(trace.size(), 32 * 4);
-    for(uint32_t i = 0; i < 32 * 4; i++) {
-        ASSERT_EQ(trace[i], std::make_tuple(true, i));
-    }
+    Trace trace;
+    
+    propagate(spikeWords.data(), spikeWords.size(), 1, trace);
+    
+    getCounts(spikeIDs, trace, true);
+    printTrace(trace);
+    //ASSERT_EQ(trace.size(), 32 * 4);
+    //for(uint32_t i = 0; i < 32 * 4; i++) {
+    //    ASSERT_EQ(trace[i], std::make_tuple(true, i));
+    //}
 }
-
+/*
 TEST(DMAEventLoop, EmptyFirstWord)
 {
     std::vector<uint32_t> spikeWords(4, 0);
     std::fill_n(spikeWords.begin() + 1, 3, 0xFFFFFFFF);
-    std::vector<std::tuple<bool, uint32_t>> trace;
+    Trace trace;
     
-    propagate(spikeWords.data(), spikeWords.size(), trace);
+    propagate(spikeWords.data(), spikeWords.size(), 1, trace);
     
-    std::sort(trace.begin(), trace.end());
-    ASSERT_EQ(trace.size(), 32 * 3);
-    for(uint32_t i = 0; i < 32 * 3; i++) {
-        ASSERT_EQ(trace[i], std::make_tuple(true, i + 32));
-    }
+    printTrace(trace);
+    //ASSERT_EQ(trace.size(), 32 * 3);
+    //for(uint32_t i = 0; i < 32 * 3; i++) {
+    //    ASSERT_EQ(trace[i], std::make_tuple(true, i + 32));
+    //}
 }
 
 TEST(DMAEventLoop, OneFirstWord)
 {
     std::vector<uint32_t> spikeWords(4, 1);
     std::fill_n(spikeWords.begin() + 1, 3, 0xFFFFFFFF);
-    std::vector<std::tuple<bool, uint32_t>> trace;
+    Trace trace;
     
-    propagate(spikeWords.data(), spikeWords.size(), trace);    
+    propagate(spikeWords.data(), spikeWords.size(), 1, trace);    
     
-    std::sort(trace.begin(), trace.end());
-    ASSERT_EQ(trace.size(), (32 * 3) + 1);
-    ASSERT_EQ(trace[0], std::make_tuple(true, 0));
-    for(uint32_t i = 0; i < 32 * 3; i++) {
-        ASSERT_EQ(trace[i + 1], std::make_tuple(true, i + 32));
-    }
+    printTrace(trace);
+    //ASSERT_EQ(trace.size(), (32 * 3) + 1);
+    //ASSERT_EQ(trace[0], std::make_tuple(true, 0));
+    //for(uint32_t i = 0; i < 32 * 3; i++) {
+    //    ASSERT_EQ(trace[i + 1], std::make_tuple(true, i + 32));
+    //}
 }
 
 TEST(DMAEventLoop, EmptyOneFirstWord)
@@ -180,32 +241,32 @@ TEST(DMAEventLoop, EmptyOneFirstWord)
     spikeWords[1] = 1;
     spikeWords[2] = 0xFFFFFFFF;
     spikeWords[3] = 0xFFFFFFFF;
-    std::vector<std::tuple<bool, uint32_t>> trace;
+    Trace trace;
     
-    propagate(spikeWords.data(), spikeWords.size(), trace);    
+    propagate(spikeWords.data(), spikeWords.size(), 1, trace);    
     
-    std::sort(trace.begin(), trace.end());
-    ASSERT_EQ(trace.size(), (32 * 2) + 1);
-    ASSERT_EQ(trace[0], std::make_tuple(true, 32));
-    for(uint32_t i = 0; i < 32 * 2; i++) {
-        ASSERT_EQ(trace[i + 1], std::make_tuple(true, i + 64));
-    }
+    printTrace(trace);
+    //ASSERT_EQ(trace.size(), (32 * 2) + 1);
+    //ASSERT_EQ(trace[0], std::make_tuple(true, 32));
+    //for(uint32_t i = 0; i < 32 * 2; i++) {
+    //    ASSERT_EQ(trace[i + 1], std::make_tuple(true, i + 64));
+    //}
 }
 
 TEST(DMAEventLoop, EmptyMiddle)
 {
     std::vector<uint32_t> spikeWords(4, 0xFFFFFFFF);
     spikeWords[2] = 0;
-    std::vector<std::tuple<bool, uint32_t>> trace;
+    Trace trace;
     
-    propagate(spikeWords.data(), spikeWords.size(), trace);
+    propagate(spikeWords.data(), spikeWords.size(), 1, trace);
     
-    std::sort(trace.begin(), trace.end());
-    ASSERT_EQ(trace.size(), 32 * 3);
-    for(uint32_t i = 0; i < 32 * 2; i++) {
-        ASSERT_EQ(trace[i], std::make_tuple(true, i));
-    }
-    for(uint32_t i = 0; i < 32; i++) {
-        ASSERT_EQ(trace[i + 64], std::make_tuple(true, i + 96));
-    }
-}
+    printTrace(trace);
+    //ASSERT_EQ(trace.size(), 32 * 3);
+    //for(uint32_t i = 0; i < 32 * 2; i++) {
+    //    ASSERT_EQ(trace[i], std::make_tuple(true, i));
+    //}
+    //for(uint32_t i = 0; i < 32; i++) {
+    //    ASSERT_EQ(trace[i + 64], std::make_tuple(true, i + 96));
+    //}
+}*/
