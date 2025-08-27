adc5cdd0bec99f0380e21a3c47f4c17e69c1a43c
neworderofjamie <neworderofjamie@gmail.com>
2025-08-27 16:14:25 +0000

    parameterize device=True if kernel looks like xilinx

--- tests/feature/conftest.py
+++ tests/feature/conftest.py
@@ -1,9 +1,14 @@
 import pytest
 
+from os import uname
+
 @pytest.fixture
 def device(request):
     return request.param
 
 def pytest_generate_tests(metafunc):
     if "device" in metafunc.fixturenames:
-        metafunc.parametrize("device", [False], indirect=True)
+        device = [False]
+        if uname().release.endswith("xilinx-zynqmp"):
+            device.append(True)
+        metafunc.parametrize("device", device, indirect=True)
