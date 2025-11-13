7e587761dc1785f935b0e18d6d432911f98e81b9
neworderofjamie <neworderofjamie@gmail.com>
2025-11-13 12:17:00 +0000

    No need to keep file object in DMA buffer

--- src/common/dma_buffer.cc
+++ src/common/dma_buffer.cc
@@ -39,28 +39,28 @@ T getIOCtl(int file, unsigned long op)
 // DMABuffer
 //----------------------------------------------------------------------------
 DMABuffer::DMABuffer(int index)
-:   m_Memory(0), m_Data(nullptr), m_PhysicalAddress(0), m_Size(0)
+:   m_UnmapData(true), m_Data(nullptr), m_PhysicalAddress(0), m_Size(0)
 {
 #ifdef __linux__ 
     LOGI << "Creating DMA buffer " << index;
 
     // Open memory
     const std::string bufferFile = "/dev/udmabuf" + std::to_string(index);
-    m_Memory = open(bufferFile.c_str(), O_RDWR | O_SYNC);
-    if(m_Memory == -1) {
+    int memory = open(bufferFile.c_str(), O_RDWR | O_SYNC);
+    if(memory == -1) {
         throw std::runtime_error(bufferFile + " open failure (" + std::to_string(errno) + " = " + strerror(errno) + ")");
     }
 
     // Read size and physical address from device
-    m_Size = getIOCtl<uint64_t>(m_Memory, U_DMA_BUF_IOCTL_GET_SIZE);
-    m_PhysicalAddress = getIOCtl<uint64_t>(m_Memory, U_DMA_BUF_IOCTL_GET_DMA_ADDR);
+    m_Size = getIOCtl<uint64_t>(memory, U_DMA_BUF_IOCTL_GET_SIZE);
+    m_PhysicalAddress = getIOCtl<uint64_t>(memory, U_DMA_BUF_IOCTL_GET_DMA_ADDR);
 
     LOGD << "\tPhysical address: " << std::hex << m_PhysicalAddress;
     LOGD << "\tSize: " << m_Size << " bytes";
 
     // Memory map data
     m_Data = reinterpret_cast<uint8_t*>(mmap(nullptr, m_Size, PROT_READ | PROT_WRITE, MAP_SHARED, 
-                                             m_Memory, 0));
+                                             memory, 0));
     if(m_Data == MAP_FAILED) {
         throw std::runtime_error("Data map failed (" + std::to_string(errno) + " = " + strerror(errno) + ")");
     }
@@ -70,7 +70,7 @@ DMABuffer::DMABuffer(int index)
 }
 //----------------------------------------------------------------------------
 DMABuffer::DMABuffer(DMABuffer &parent, uint64_t physicalStartAddress, uint64_t physicalEndAddress)
-:   m_Memory(0)
+:   m_UnmapData(false), m_Data(nullptr), m_PhysicalAddress(0), m_Size(0)
 {
 #ifdef __linux__ 
     LOGI << "Creating child DMA buffer with target physical memory region: " << std::hex << physicalStartAddress << " - " << physicalEndAddress;
@@ -102,13 +102,9 @@ DMABuffer::DMABuffer(DMABuffer &parent, uint64_t physicalStartAddress, uint64_t 
 DMABuffer::~DMABuffer()
 {
 #ifdef __linux__ 
-    // If this is parent DMA buffer
-    if(m_Memory) {
-        // Unmap memory
+    // Unmap data if required
+    if(m_UnmapData) {
         munmap(m_Data, m_Size);
-    
-        // Close memory device
-        close(m_Memory);
     }
 #endif  // __linux__
 }
