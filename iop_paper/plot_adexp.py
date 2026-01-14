c8cdfb53a781d6042fe93a38a6a69bb8ba478a3e
neworderofjamie <neworderofjamie@gmail.com>
2026-01-14 16:52:26 +0000

    re-order axes in adexp figure

--- iop_paper/plot_adexp.py
+++ iop_paper/plot_adexp.py
@@ -12,16 +12,19 @@ fig, axes = plt.subplots(2, 3, sharex="col", sharey="row", figsize=(plot_setting
 
 timesteps = np.arange(0.0, 2000 * 0.1, 0.1)
 
+order = ["tonic", "adaptation", "initial burst",
+         "transient spiking", "regular bursting", "delayed accelerating"]
+    
 fixed_point_one = 2 ** 11
-for i, (a, n) in enumerate(zip(axes.flatten(), param_sets.keys())):
+for i, (a, o) in enumerate(zip(axes.flatten(), order)):
     letter = chr(ord("A") + i)
-    print(f"{letter} = {n}")
+    print(f"{letter} = {o}")
     
     a.set_title(letter, loc="left")
     
-    fenn_rn_actor = a.plot(timesteps, fenn_data[f"{n}_v_rn"][:,1] / (fixed_point_one * 0.01))[0]
-    fenn_rs_actor = a.plot(timesteps, fenn_data[f"{n}_v_rs"][:,1] / (fixed_point_one * 0.01))[0]
-    genn_actor = a.plot(timesteps, genn_data[f"{n}_v"][:,0], color="gray", linestyle="--")[0]
+    fenn_rn_actor = a.plot(timesteps, fenn_data[f"{o}_v_rn"][:,1] / (fixed_point_one * 0.01))[0]
+    fenn_rs_actor = a.plot(timesteps, fenn_data[f"{o}_v_rs"][:,1] / (fixed_point_one * 0.01))[0]
+    genn_actor = a.plot(timesteps, genn_data[f"{o}_v"][:,0], color="gray", linestyle="--")[0]
     
     a.xaxis.grid(False)
     sns.despine(ax=a)
@@ -31,6 +34,7 @@ for i in range(axes.shape[0]):
 for j in range(axes.shape[1]):
     axes[-1, j].set_xlabel("Time [ms]")
 
+axes[0,0].set_xlim((0, 100))
 fig.legend([fenn_rn_actor, fenn_rs_actor, genn_actor], ["FeNN (RN)", "FeNN (RS)", "GeNN reference"], 
            ncol=3, loc="lower center", frameon=False)
 
