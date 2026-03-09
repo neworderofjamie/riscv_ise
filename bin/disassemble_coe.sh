4e24f84d3b6b097f46e199786a67fc0b8f0d65b6
neworderofjamie <neworderofjamie@gmail.com>
2026-03-09 13:22:44 +0000

    include addresses in disassembly

--- bin/disassemble_coe.sh
+++ bin/disassemble_coe.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+ADDRESS=0
 tail -n +3 $1 | while read LINE
 do
     LINE_NO_COMMA="${LINE%,}"
     LINE_NO_SUFFIX="${LINE_NO_COMMA%;}"
+    printf "%x " $ADDRESS
     ./disassembler_cli 0x$LINE_NO_SUFFIX
+    ADDRESS=$[ADDRESS + 4]
    # do something with $line here
 done
