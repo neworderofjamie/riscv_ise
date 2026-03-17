3748748ad76d3520aae3ef613629bf8d6d93840d
neworderofjamie <neworderofjamie@gmail.com>
2026-03-17 16:48:26 +0000

    whitespace

--- include/common/dma_controller.h
+++ include/common/dma_controller.h
@@ -18,6 +18,7 @@ class COMMON_EXPORT DMAController
 {
 public:
     DMAController(const std::string &uioName);
+
     //------------------------------------------------------------------------
     // Public API
     //------------------------------------------------------------------------
