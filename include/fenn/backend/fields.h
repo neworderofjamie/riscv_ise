871051133fe4f43756d43a0f0bca1395cb4a81bc
neworderofjamie <neworderofjamie@gmail.com>
2026-03-18 17:37:19 +0000

    tidied up MergedFields a little with default template parameters, better names and removed **TODO**

--- include/fenn/backend/fields.h
+++ include/fenn/backend/fields.h
@@ -15,6 +15,7 @@ namespace Frontend
 {
 class ArrayBase;
 class DeviceBase;
+class Process;
 class State;
 }
 
@@ -28,14 +29,14 @@ class MergedFields
 public:
     using FieldValue = std::variant<int32_t, uint32_t>;
 
-    template<typename P>
+    template<typename P = Frontend::Process>
     using GetFieldConstantFunc = std::function<FieldValue(size_t, std::shared_ptr<const P>)>;
 
-    template<typename P>
+    template<typename P = Frontend::Process>
     using GetFieldPointerFunc = std::function<Frontend::ArrayBase*(const Frontend::DeviceBase&, 
                                                                    std::shared_ptr<const P>)>;
 
-    template<typename P>
+    template<typename P = Frontend::Process>
     using GetFieldValueFunc = std::variant<GetFieldConstantFunc<P>, GetFieldPointerFunc<P>>;
 
     MergedFields() : m_NextFieldOffset(0)
@@ -44,46 +45,44 @@ public:
     //----------------------------------------------------------------------------
     // Public API
     //----------------------------------------------------------------------------
-    // **TODO** untemplate
     template<typename P>
     uint32_t addField(GetFieldConstantFunc<P> getFieldConstantFn, uint32_t fieldSize = 4)
     {
         // Gather state from all merged processes and assign to field
-        m_FieldOffsets.emplace_back(m_NextFieldOffset, 
-                                    [getFieldConstantFn]
-                                    (size_t d, std::shared_ptr<const Frontend::Process> p)
-                                    {
-                                        return getFieldConstantFn(d, std::static_pointer_cast<const P>(p));
-                                    });
+        m_Fields.emplace_back(m_NextFieldOffset, 
+                              [getFieldConstantFn]
+                              (size_t d, std::shared_ptr<const Frontend::Process> p)
+                              {
+                                  return getFieldConstantFn(d, std::static_pointer_cast<const P>(p));
+                              });
 
         // Update next field offset
         m_NextFieldOffset += fieldSize;
 
         // Return offset of new fiel,d
-        return m_FieldOffsets.back().first;
+        return m_Fields.back().first;
     }
 
-    // **TODO** untemplate
     template<typename P>
     uint32_t addField(GetFieldPointerFunc<P> getFieldPointerFn, uint32_t fieldSize = 4)
     {
         // Gather state from all merged processes and assign to field
-        m_FieldOffsets.emplace_back(m_NextFieldOffset, 
-                                    [getFieldPointerFn]
-                                    (const Frontend::DeviceBase &d, std::shared_ptr<const Frontend::Process> p)
-                                    {
-                                        return getFieldPointerFn(d, std::static_pointer_cast<const P>(p));
-                                    });
+        m_Fields.emplace_back(m_NextFieldOffset, 
+                              [getFieldPointerFn]
+                              (const Frontend::DeviceBase &d, std::shared_ptr<const Frontend::Process> p)
+                              {
+                                  return getFieldPointerFn(d, std::static_pointer_cast<const P>(p));
+                              });
 
         // Update next field offset
         m_NextFieldOffset += fieldSize;
 
         // Return offset of new fiel,d
-        return m_FieldOffsets.back().first;
+        return m_Fields.back().first;
     }
 
-    const auto &getFieldOffsets() const{ return m_FieldOffsets; }
-    const uint32_t getSize() const{ return m_NextFieldOffset; }
+    const auto &getFields() const{ return m_Fields; }
+    uint32_t getSize() const{ return m_NextFieldOffset; }
 
 private:
     
@@ -92,7 +91,7 @@ private:
     // Members
     //----------------------------------------------------------------------------
     uint32_t m_NextFieldOffset;
-    std::vector<std::pair<uint32_t, GetFieldValueFunc<const Frontend::Process>>> m_FieldOffsets;
+    std::vector<std::pair<uint32_t, GetFieldValueFunc<const Frontend::Process>>> m_Fields;
 };
 }
 
