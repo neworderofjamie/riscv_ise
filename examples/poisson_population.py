723d5178ecb4863b66ce1ce683e21e5ed92f4fbd
cgoldman123 <carter.goldman19@gmail.com>
2026-06-03 16:18:02 +0100

    configured both primary and extra input neuron populations following fusi experiment

--- examples/poisson_population.py
+++ examples/poisson_population.py
@@ -16,13 +16,14 @@ import matplotlib.pyplot as plt
 # One trial can consist of several timesteps, determining how many
 # times the processes are run
 num_timesteps = 1
-input_shape = 10
+extra_input_shape = 10
+primary_input_shape = 1
 output_shape = 1
 num_trials = 1000
 # Pretend as if 1000 trials correspond to 1 second
 trials_per_second = 1000
 
-input_output_shape = (input_shape, output_shape)
+extra_input_output_shape = (extra_input_shape, output_shape)
 
 parser = ArgumentParser("Poisson population")
 parser.add_argument("--device", action="store_true", help="Run model on FeNN hardware")
@@ -39,17 +40,19 @@ v_threshold = 1
 postsyn_v_thresh_LTP = v_threshold*.8
 
 # weird things happen when prob_spike can't be expressed as a fraction with denom=64
-input = Bernoulli(Shape(input_shape),prob_spike=(2**4)/(2**6),record_timesteps=1,fixed_point=num_fixed_point_bits,name="input")
+primary_input = Bernoulli(Shape(primary_input_shape),prob_spike=(2**3)/(2**6),record_timesteps=1,fixed_point=num_fixed_point_bits,name="primary_input")
+extra_input = Bernoulli(Shape(extra_input_shape),prob_spike=(2**4)/(2**6),record_timesteps=1,fixed_point=num_fixed_point_bits,name="extra_input")
 output = LIF_STDP(output_shape, alpha=.01, v_thresh=v_threshold, v_reset=0, record_timesteps=1, fixed_point=num_fixed_point_bits, dt=1, name="output")
-input_output = Linear(input.out_spikes, output.i, "s9_6_sat_t", name="input_output")
+
+extra_input_output = Linear(extra_input.out_spikes, output.i, "s9_6_sat_t", name="extra_input_output")
+primary_input_output = Linear(primary_input.out_spikes, output.i, "s9_6_sat_t", name="primary_input_output")
 
 # Note that memset will clear the variable at the end of the trial!!
 # v_zero = Memset(output.v)
 
 # Group processes
-# neuron_update_processes = ProcessGroup([input.process, output.process], PerformanceCounter() if args.time else None)
-neuron_update_processes = ProcessGroup([input.process, output.process], PerformanceCounter() if args.time else None)
-synapse_update_processes = ProcessGroup([input_output.process], PerformanceCounter() if args.time else None)
+neuron_update_processes = ProcessGroup([extra_input.process, primary_input.process, output.process], PerformanceCounter() if args.time else None)
+synapse_update_processes = ProcessGroup([extra_input_output.process, primary_input_output.process], PerformanceCounter() if args.time else None)
 # zero_processes = ProcessGroup([v_zero.process], PerformanceCounter() if args.time else None)
 
 # Create backend
@@ -76,18 +79,22 @@ runtime = Runtime(model, backend)
 runtime.allocate()
 
 # Load weights
-# print(np.asarray(runtime.get_array(input_output.weight).host_view).shape)
-# input_output.weight variable is an 80 element array where every 8*ith element
+# print(np.asarray(runtime.get_array(extra_input_output.weight).host_view).shape)
+# extra_input_output.weight variable is an 80 element array where every 8*ith element
 # corresponds to the i-th synapse (e.g., index 16 is the third synapse). Each 
 # element corresponds to a fixed point int with 6 fractional bits, so 32 is .5,
 # 64 is 1, 128 is 2.
-weights = np.zeros(input_shape*8,dtype='uint64')*64
+extra_input_weights = np.zeros(extra_input_shape*8,dtype='uint64')*64
 for idx in range(0,80,8):
-    weights[idx]=2**1
-
-copy_and_push(weights, input_output.weight, runtime)
+    extra_input_weights[idx]=2**1
+copy_and_push(extra_input_weights, extra_input_output.weight, runtime)
 # I could instead do 
-# copy_and_push(np.ones(input_shape*64,dtype='uint8'), input_output.weight, runtime)
+# copy_and_push(np.ones(extra_input_shape*64,dtype='uint8'), extra_input_output.weight, runtime)
+
+primary_input_weights = np.zeros(primary_input_shape*8,dtype='uint64')*64
+primary_input_weights[0]=2**1
+copy_and_push(primary_input_weights, primary_input_output.weight, runtime)
+
 
 # Zero remaining state
 zero_and_push(output.v, runtime)
@@ -101,37 +108,50 @@ if args.time:
 # Set instructions
 runtime.set_instructions(code)
 
-# Initialize variables to hold input spike and output voltages
-input_spike_array, input_spike_view = get_array_view(runtime, input.out_spikes, np.uint32)
+# Initialize variables to hold extra_input spike and output voltages
+primary_input_spike_array, primary_input_spike_view = get_array_view(runtime, primary_input.out_spikes, np.uint32)
+extra_input_spike_array, extra_input_spike_view = get_array_view(runtime, extra_input.out_spikes, np.uint32)
 output_v_array, output_v_view = get_array_view(runtime, output.v, np.int16)
 output_spike_array, output_spike_view = get_array_view(runtime, output.out_spikes, np.int16)
 
-neural_activity = [[0] * 3 for i in range(num_trials)]
+neural_activity = [[0] * 4 for i in range(num_trials)]
 
 for i in range(num_trials):
     # run the model
     runtime.run()
 
-    # Copy input spike array and output voltage from device
-    input_spike_array.pull_from_device()
+    # Copy extra_input spike array and output voltage from device
+    extra_input_spike_array.pull_from_device()
+    primary_input_spike_array.pull_from_device()
     output_v_array.pull_from_device()
     output_spike_array.pull_from_device()
-    # print("Input spikes: ", input_spike_view)
+    # print("extra_input spikes: ", extra_input_spike_view)
     # print("Output voltages: ", output_v_view[0]/(2**num_fixed_point_bits))
     # print("Output spikes: ", output_spike_view[0])
     # print()
-    neural_activity[i][0] = input_spike_view[0]
+    neural_activity[i][0] = extra_input_spike_view[0]
     neural_activity[i][1] = output_v_view[0]/(2**num_fixed_point_bits)
     neural_activity[i][2] = output_spike_view[0]
+    neural_activity[i][3] = primary_input_spike_view[0]
 
-presyn_spikes = [neural_act[0] for neural_act in neural_activity]
+
+extra_presyn_spikes = [neural_act[0] for neural_act in neural_activity]
 postsyn_voltages = [neural_act[1] for neural_act in neural_activity]
 postsyn_spikes = [neural_act[2] for neural_act in neural_activity]
+primary_presyn_spikes = [neural_act[3] for neural_act in neural_activity]
 
 postsyn_spike_rate = np.sum(postsyn_spikes) / (num_trials/trials_per_second)
 
 fig, axes = plt.subplots(4, sharex=True, figsize=(12, 5))
 fig.tight_layout(pad=2.0)
+
+# plot presyn spikes
+presyn_spike_times = np.where(np.array(primary_presyn_spikes) == 1)[0]
+for s in presyn_spike_times:
+    axes[0].set_xlim((0,num_trials))
+    axes[0].axvline(s)
+axes[0].title.set_text("Presynaptic spikes")
+
 
 # plot postsyn V
 axes[2].title.set_text('Postsynaptic voltage V(t) (Spike rate: ' + str(postsyn_spike_rate) + " Hz)")
