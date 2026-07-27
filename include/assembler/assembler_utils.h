e9dbeec354016c9a9bc08e6d3ca4d56f6075f0b7
neworderofjamie <neworderofjamie@gmail.com>
2026-07-27 15:21:28 +0100

    fixed typo

--- include/assembler/assembler_utils.h
+++ include/assembler/assembler_utils.h
@@ -74,13 +74,7 @@ ASSEMBLER_EXPORT void generateAddUint64(CodeGenerator &c, ScalarRegisterAllocato
 
 //! Generate code to wait until target cycles has elapsed since cycle count stored in registers
 ASSEMBLER_EXPORT void generateWaitElapsedCycles(CodeGenerator &c, ScalarRegisterAllocator &scalarRegisterAllocator,
-                                                Reg lowStartCycles, Reg highStartCycles, uint32-------+-++++++++++++++++++++
-                                                
-                                                
-                                                
-                                                
-                                                
-                                                ++++++_t targetCycles);
+                                                Reg lowStartCycles, Reg highStartCycles, uint32_t targetCycles);
 
 //! Generate code to initiate a DMA write
 ASSEMBLER_EXPORT void generateDMAStartWrite(CodeGenerator &c, Reg destination, Reg source, Reg size);
