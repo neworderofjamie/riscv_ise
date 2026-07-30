2ca9ca825604e56b570f8847b9d580dba295905d
Jamie Knight <neworderofjamie@gmail.com>
2026-07-30 10:38:32 +0100

    updated script to convert mnist to new spike format

--- bin/generate_mnist.py
+++ bin/generate_mnist.py
@@ -14,18 +14,42 @@ padded_size = int(np.ceil(test_images.shape[1] / 32)) * 32
 
 spikes = []
 for t in test_images:
+    # Calculate log-latency from intensity
     times = np.round(20.0 * np.log(t / (t - 51))).astype(int)
-    times_in_range = (t > 51) & (times < 79)
+    
+    # Get IDs of neurons which should spike
+    neuron_ids = np.where((t > 51) & (times < 79))[0]
+    
+    # Order by time
+    times = times[neuron_ids]
+    order = np.argsort(times)
+    times = times[order]
+    neuron_ids = neuron_ids[order].astype(np.uint16)
+    
+    # Count number of spikes in each timestep and use this to split neuron ids
+    num_spikes_per_time = np.cumsum(np.bincount(times))
+    neuron_ids_per_time = np.split(neuron_ids, num_spikes_per_time)
+    
+    # Concatenate timestamps onto each non-empty group of neuron ids
+    neuron_ids_per_time = [np.concatenate(((t | 1 << 15,), n))
+                           for t, n in enumerate(neuron_ids_per_time)
+                           if len(n) > 0]
 
-    spike_event_histogram = np.zeros((NUM_TIMESTEPS, test_images.shape[1]), dtype=bool)
-    spike_event_histogram[times[times_in_range], times_in_range] = 1
-    spike_event_histogram = np.pad(spike_event_histogram, ((0, 0), (0, (padded_size - test_images.shape[1]))))
-    spikes.append(np.packbits(spike_event_histogram, axis=1, bitorder="little"))
+    # Rejoin into single array
+    neuron_ids_per_time = np.concatenate(neuron_ids_per_time)
+    
+    spikes.append(neuron_ids_per_time)
 
-spikes = np.stack(spikes)
+# Calculate maximum spikes per-image
+max_spikes_per_image = max(len(s) for s in spikes)
+print(f"Maximum spikes per-image = {max_spikes_per_image}")
+
+# Pad all spike arrays to this length with uint16_max
+spikes = [np.pad(s, (0, max_spikes_per_image - len(s)), constant_values=0xFFFF) for s in spikes]
+
+# Concatenate and write to file
+spikes = np.concatenate(spikes)
 spikes.tofile("mnist_spikes.bin")
-#test_times_int = np.round(test_times).astype(np.int16)
-#test_times_int.tofile("mnist_times.bin")
 
 test_labels_int = np.round(test_labels).astype(np.int16)
 test_labels_int.tofile("mnist_labels.bin")
