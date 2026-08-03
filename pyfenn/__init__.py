59c1a86f2b951753a8082f2ac7a20ac1e3cbfed1
neworderofjamie <neworderofjamie@gmail.com>
2026-08-03 17:22:52 +0100

    fixed typos

--- pyfenn/__init__.py
+++ pyfenn/__init__.py
@@ -2,7 +2,7 @@
 from importlib import metadata
 
 # pyfenn interface
-from . import fenn_backend, models, nir_import, utils
+from . import fenn_backend#, models, nir_import, utils
 
 __all__ = ["fenn_backend", "models", "nir_import", "utils"]
 
--- pyfenn/fenn_backend.py
+++ pyfenn/fenn_backend.py
@@ -1,8 +1,8 @@
 from ._fenn_backend import (BroadcastProcess, DenseEventPropagationProcess, 
-                            EventSourceChannel, EventSinkBuffer, 
-                            EventSourceBuffer, MemsetProcess, 
-                            NeuronUpdateProcess, RNGInitProcess, 
-                            RoundingMode, RuntimeHW, RuntimeSim, Variable,
-                            disassemble)
+                            EventChannel, EventSinkBuffer, EventSourceBuffer, 
+                            MemsetProcess, NeuronUpdateProcess, 
+                            RNGInitProcess, RoundingMode, RuntimeHW, 
+                            RuntimeSim, Variable, disassemble)
+                            
 from ._frontend import (NumericValue, PlogSeverity, ProcessGroup, 
                         ResolvedType, Shape, UnresolvedType, init_logging)
