27a79b9ddb0761ad4c4ec7b661029e9d9ff4c403
neworderofjamie <neworderofjamie@gmail.com>
2026-08-13 15:50:03 +0100

    started resurrecting PyFeNN tests

--- tests/pyfenn/test_event_propagation.py
+++ tests/pyfenn/test_event_propagation.py
@@ -2,46 +2,38 @@ import numpy as np
 import pytest
 from typing import Optional
 
-from pyfenn import (BackendFeNNHW, BackendFeNNSim, EventContainer,
-                    EventPropagationProcess, MemsetProcess, Model,
-                    NeuronUpdateProcess, PlogSeverity, ProcessGroup, Runtime,
-                    Variable)
+import pyfenn.fenn_backend as backend
 
-from pyfenn import disassemble, init_logging
-from pyfenn.models import Linear, Memset
+from pyfenn.models import DenseLinear, Memset, SparseLinear
 from pyfenn.utils import (build_delay_weights, build_sparse_connectivity,
-                          ceil_divide, copy_and_push, get_array_view)
+                          build_spike_array, copy_and_push, get_views)
                           
 
 
 class PostNeuron:
-    def __init__(self, shape, num_i_timesteps, num_x_timesteps, name: str = ""):
+    def __init__(self, backend, shape, num_i_timesteps, num_x_timesteps, name: str = ""):
         self.shape = shape
-        self.i = Variable(self.shape, "int16_t", num_i_timesteps, name=f"{name}_I")
-        self.x = Variable(self.shape, "int16_t", num_x_timesteps, name=f"{name}_X")
-        self.process = NeuronUpdateProcess(
+        self.i = backend.Variable((num_i_timesteps,) + self.shape, "int16_t", name=f"{name}_I")
+        self.x = backend.Variable((num_x_timesteps,) + self.shape, "int16_t", name=f"{name}_X")
+        self.process = backend.NeuronUpdateProcess(
             """
             X = I;
             I = 0;
             """, 
-            {}, {"I": self.i, "X": self.x}, {}, name)
+            {"I": backend.SlicedVariable(self.i, True), 
+             "X": backend.SlicedVariable(self.x, True)}, {},
+            name=name)
 
 @pytest.mark.parametrize("use_dram_for_weights", [True, False])
-def test_forward(device, use_dram_for_weights):
-    init_logging(PlogSeverity.INFO)
+def test_forward(device, use_dram_for_weights):    
+    log_appender = backend.ConsoleAppender()
+    backend.init_logging(log_appender, backend.PlogSeverity.INFO)
 
-    # Create one-hot pattern of spikes to decode
-    spikes = np.identity(16, dtype=bool)
+    # Build spike array
+    spike_neuron_ids = np.arange(16)
+    spike_timesteps = np.arange(16)
+    spike_array = build_spike_array(spike_timesteps, spike_neuron_ids)
 
-    # Pack along neurons axis
-    spikes = np.packbits(spikes, axis=1, bitorder="little")
-
-    # Pad neuron axes to 4 bytes
-    spikes = np.pad(spikes, ((0, 0), (0, 4 - spikes.shape[1])))
-
-    # Convert to uint32 and flatten
-    spikes = spikes.view(np.uint32).flatten()
-    
     # Build sparse connectivity
     j = np.arange(4)
     j_value = 1 << j
@@ -57,61 +49,54 @@ def test_forward(device, use_dram_for_weights):
     conn = build_sparse_connectivity(conn, 1, 3)
 
     # Create input spike container
-    input_spikes = EventContainer(16, 16)
+    input_spikes = backend.EventSourceBuffer((16,), 16, name="input_events")
 
     # Create one output neuron pop with sparse decoder population
-    sparse_n_pop = PostNeuron(4, 1, 17, "SparseNPop")
-    dense_n_pop = PostNeuron(4, 1, 17, "DenseNPop")
+    sparse_n_pop = PostNeuron(backend, (4,), 1, 17, "SparseNPop")
+    dense_n_pop = PostNeuron(backend, (4,), 1, 17, "DenseNPop")
 
-    input_sparse = Linear(input_spikes, sparse_n_pop.i, "int16_t", 
-                          max_row_length=4, num_sparse_connectivity_bits=3,
-                          name="input_sparse")
-    input_dense = Linear(input_spikes, dense_n_pop.i, "int16_t", name="input_dense")
+    input_sparse = SparseLinear(backend, input_spikes, backend.SlicedVariable(sparse_n_pop.i, True), 
+                                "int16_t", max_row_length=4, num_sparse_connectivity_bits=3,
+                                name="input_sparse")
+    input_dense = DenseLinear(backend, input_spikes, backend.SlicedVariable(dense_n_pop.i, True), 
+                              "int16_t", name="input_dense")
 
     # Initialisation
-    sparse_zero = Memset(sparse_n_pop.i)
-    dense_zero = Memset(dense_n_pop.i)
+    sparse_zero = Memset(backend, sparse_n_pop.i)
+    dense_zero = Memset(backend, dense_n_pop.i)
     
     # Group processes
-    init_processes = ProcessGroup([sparse_zero.process, dense_zero.process])
-    neuron_update_processes = ProcessGroup([sparse_n_pop.process, dense_n_pop.process])
-    synapse_update_processes = ProcessGroup([input_sparse.process, input_dense.process])
-    
-    # Create backend
-    backend_kwargs = {"use_dram_for_weights": use_dram_for_weights}
-    backend = BackendFeNNHW(**backend_kwargs) if device else BackendFeNNSim(**backend_kwargs)
+    init_processes = backend.ProcessGroup([sparse_zero.process, dense_zero.process])
+    neuron_update_processes = backend.ProcessGroup([sparse_n_pop.process, dense_n_pop.process])
+    synapse_update_processes = backend.ProcessGroup([input_sparse.process, input_dense.process])
 
-    # Create model
-    model = Model([init_processes, neuron_update_processes, synapse_update_processes],
-                   backend)
+    # Create simulation kernel
+    sim_kernel = backend.SimulationLoopKernel(
+        16, [synapse_update_processes, neuron_update_processes],
+        [init_processes], [])
 
-    # Generate sim code
-    code = backend.generate_simulation_kernel([synapse_update_processes, neuron_update_processes],
-                                              [init_processes], [],
-                                              16, model)
-    
+
     # Create runtime
-    runtime = Runtime(model, backend)
+    runtime_params = {"use_dram_for_weights": use_dram_for_weights}
+    runtime = (backend.RuntimeHW([sim_kernel], 1, **runtime_params) if device 
+            else backend.RuntimeSim([sim_kernel], 1, **runtime_params))
 
     # Allocate memory for model
     runtime.allocate()
 
     # Initialise weights
-    copy_and_push(spikes, input_spikes, runtime)
+    copy_and_push(spike_array, input_spikes, runtime)
     copy_and_push(conn.flatten(), input_sparse.weight, runtime)
     copy_and_push(dense.flatten(), input_dense.weight, runtime)
    
-    # Set sim instructions
-    runtime.set_instructions(code)
-
     # Simulate
-    runtime.run()
+    runtime.run(sim_kernel)
 
     # Loop through output processes
     output_place_values = 2 ** np.arange(4)
     for p in [sparse_n_pop, dense_n_pop]:
-        x_array, x_view = get_array_view(runtime, p.x, np.int16, (17, 32))
-        x_array.pull_from_device()
+        x_view = get_views(runtime, p.x)
+        runtime.pull_state_from_device(p.x)
         
         # Remove first timestep and padding neurons; and convert to bool
         x_view = x_view[1:,:4].astype(bool)
@@ -124,7 +109,8 @@ def test_forward(device, use_dram_for_weights):
     
 @pytest.mark.parametrize("use_dram_for_weights", [True, False])
 def test_forward_den_delay(device, use_dram_for_weights):
-    init_logging(PlogSeverity.INFO)
+    log_appender = backend.ConsoleAppender()
+    backend.init_logging(log_appender, backend.PlogSeverity.INFO)
 
     num_pre = 10
     num_post = 4
