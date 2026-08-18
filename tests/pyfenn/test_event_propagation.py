2c8f1bb91218c1e63c9541aba09631d321f0099d
Jamie Knight <neworderofjamie@gmail.com>
2026-08-18 19:40:36 +0100

    fixed up some syntax errors in dendritically delayed event propagation test

--- tests/pyfenn/test_event_propagation.py
+++ tests/pyfenn/test_event_propagation.py
@@ -4,23 +4,42 @@ from typing import Optional
 
 import pyfenn.fenn_backend as backend
 
-from pyfenn.models import DenseLinear, Memset, SparseLinear
+from pyfenn.models import DelayLinear, DenseLinear, Memset, SparseLinear
 from pyfenn.utils import (build_delay_weights, build_sparse_connectivity,
                           build_spike_array, copy_and_push, get_views)
                           
 
 
 class PostNeuron:
-    def __init__(self, backend, shape, num_i_timesteps, num_x_timesteps, name: str = ""):
+    def __init__(self, backend, shape, num_x_timesteps, name: str = ""):
         self.shape = shape
-        self.i = backend.Variable((num_i_timesteps,) + self.shape, "int16_t", name=f"{name}_I")
+        self.i = backend.Variable(self.shape, "int16_t", name=f"{name}_I")
         self.x = backend.Variable((num_x_timesteps,) + self.shape, "int16_t", name=f"{name}_X")
         self.process = backend.NeuronUpdateProcess(
             """
             X = I;
             I = 0;
             """, 
-            {"I": backend.SlicedVariable(self.i, True), 
+            {"I": self.i, 
+             "X": backend.SlicedVariable(self.x, True)}, {},
+            name=name)
+
+class PostNeuronDelay:
+    def __init__(self, backend, shape, num_x_timesteps, num_delay_timesteps, name: str = ""):
+        self.shape = shape
+        self.i = backend.Variable(self.shape, "int16_t", name=f"{name}_I")
+        self.x = backend.Variable((num_x_timesteps,) + self.shape, "int16_t", name=f"{name}_X")
+        self.den_delay_buffer = backend.Variable((num_delay_timesteps,) + self.shape, 
+                                                 "int16_t", name=f"{name}_den_delay")
+
+        self.den_delay_update_process = backend.DendriticDelayUpdateProcess(
+            self.den_delay_buffer, self.i, name="f{name}_den_delay_update")
+        self.process = backend.NeuronUpdateProcess(
+            """
+            X = I;
+            I = 0;
+            """, 
+            {"I": self.i, 
              "X": backend.SlicedVariable(self.x, True)}, {},
             name=name)
 
@@ -48,17 +67,17 @@ def test_forward(device, use_dram_for_weights):
     # Convert into internal format
     conn = build_sparse_connectivity([conn], 1, 3)[0]
 
-    # Create input spike container
+    # Create input spike source buffer
     input_spikes = backend.EventSourceBuffer((16,), len(spike_array), name="input_events")
 
     # Create one output neuron pop with sparse decoder population
-    sparse_n_pop = PostNeuron(backend, (4,), 1, 17, "SparseNPop")
-    dense_n_pop = PostNeuron(backend, (4,), 1, 17, "DenseNPop")
+    sparse_n_pop = PostNeuron(backend, (4,), 17, "SparseNPop")
+    dense_n_pop = PostNeuron(backend, (4,), 17, "DenseNPop")
 
-    input_sparse = SparseLinear(backend, input_spikes, backend.SlicedVariable(sparse_n_pop.i, True), 
+    input_sparse = SparseLinear(backend, input_spikes, sparse_n_pop.i, 
                                 "int16_t", max_row_length=conn.shape[1], num_sparse_connectivity_bits=3,
                                 name="input_sparse")
-    input_dense = DenseLinear(backend, input_spikes, backend.SlicedVariable(dense_n_pop.i, True), 
+    input_dense = DenseLinear(backend, input_spikes, dense_n_pop.i, 
                               "int16_t", name="input_dense")
 
     # Initialisation
@@ -116,80 +135,64 @@ def test_forward_den_delay(device, use_dram_for_weights):
     num_post = 4
     num_delay_bits = 5
 
-    num_pre_vecs = ceil_divide(num_pre, 32)
-    num_post_vecs = ceil_divide(num_post, 32)
-
-    # Create one-hot pattern of spikes to decode
-    spikes = np.identity(num_pre, dtype=bool)
-
-    # Pack along neurons axis
-    spikes = np.packbits(spikes, axis=1, bitorder="little")
-
-    # Pad neuron axes to 4 bytes
-    spikes = np.pad(spikes, ((0, 0), (0, (num_pre_vecs * 4) - spikes.shape[1])))
-
-    # Convert to uint32 and flatten
-    spikes = spikes.view(np.uint32).flatten()
+    # Build spike array
+    spike_neuron_ids = np.arange(num_pre)
+    spike_timesteps = np.arange(num_pre)
+    spike_array = build_spike_array(spike_timesteps, spike_neuron_ids)
 
     # Build combined
     delays = np.reshape(np.arange(num_pre - 1, -1, -1), (num_pre, 1))
     delay_weights = build_delay_weights(np.ones_like(delays), delays, num_delay_bits)
     delay_weights = np.tile(delay_weights, (1, num_post))
-    delay_weights = np.pad(delay_weights, ((0, 0), (0, (num_post_vecs * 32) - delay_weights.shape[1])))
 
-    # Create input spike container
-    input_spikes = EventContainer(num_pre, num_pre)
+    # Create input spike source buffer
+    input_spikes = backend.EventSourceBuffer((num_pre,), len(spike_array), name="input_events")
 
     # Create one output neuron pop
-    dense_n_pop = PostNeuron(num_post, 2**(num_delay_bits - 1), num_pre + 1, "DenseNPop")
+    pop = PostNeuronDelay(backend, (num_post,), num_pre + 1, 2**(num_delay_bits - 1), "DenseNPop")
 
     # Create delayed connection from input spikes to dense
-    input_dense = Linear(input_spikes, dense_n_pop.i, "int16_t", 
-                         num_delay_bits=num_delay_bits, name="input_dense")
+    input_pop = DelayLinear(backend, input_spikes, pop.den_delay_buffer, "int16_t", 
+                            num_delay_bits=num_delay_bits, name="input_dense")
 
     # Initialisation
-    dense_zero = Memset(dense_n_pop.i)
+    i_zero = Memset(backend, pop.i)
+    den_delay_zero = Memset(backend, pop.den_delay_buffer)
     
     # Group processes
-    init_processes = ProcessGroup([dense_zero.process])
-    neuron_update_processes = ProcessGroup([dense_n_pop.process])
-    synapse_update_processes = ProcessGroup([input_dense.process])
+    init_processes = backend.ProcessGroup([i_zero.process, den_delay_zero.process])
+    neuron_update_processes = backend.ProcessGroup([pop.process])
+    den_delay_update_processes = backend.ProcessGroup([pop.den_delay_update_process], name="den-delay update")
+    synapse_update_processes = backend.ProcessGroup([input_pop.process])
     
-    # Create backend
-    backend_kwargs = {"use_dram_for_weights": use_dram_for_weights}
-    backend = BackendFeNNHW(**backend_kwargs) if device else BackendFeNNSim(**backend_kwargs)
+    # Create simulation kernel
+    sim_kernel = backend.SimulationLoopKernel(
+        num_pre, [synapse_update_processes, den_delay_update_processes, neuron_update_processes],
+        [init_processes], [])
 
-    # Create model
-    model = Model([init_processes, neuron_update_processes, synapse_update_processes],
-                   backend)
 
-    # Generate sim code
-    code = backend.generate_simulation_kernel([synapse_update_processes, neuron_update_processes],
-                                              [init_processes], [],
-                                              num_pre, model)
     # Create runtime
-    runtime = Runtime(model, backend)
+    runtime_params = {"use_dram_for_weights": use_dram_for_weights}
+    runtime = (backend.RuntimeHW([sim_kernel], 1, **runtime_params) if device 
+               else backend.RuntimeSim([sim_kernel], 1, **runtime_params))
 
     # Allocate memory for model
     runtime.allocate()
 
     # Initialise weights
-    copy_and_push(spikes, input_spikes, runtime)
-    copy_and_push(delay_weights.flatten(), input_dense.weight, runtime)
-
-     # Set sim instructions
-    runtime.set_instructions(code)
+    copy_and_push(spike_array, input_spikes, runtime)
+    copy_and_push(delay_weights, input_pop.weight, runtime)
 
     # Simulate
-    runtime.run()
+    runtime.run(sim_kernel)
 
     # Simulate for 11 timesteps
     correct = np.reshape(np.zeros(num_pre), (num_pre, 1))
     correct[-1] = num_pre
 
-    for p in [dense_n_pop]:
-        x_array, x_view = get_array_view(runtime, p.x, np.int16, (num_pre + 1, num_post_vecs * 32))
-        x_array.pull_from_device()
+    for p in [pop]:
+        x_view = get_views(runtime, p.x)[0]
+        runtime.pull_state_from_device(p.x)
 
         # Remove first timestep and padding neurons
         x_view = x_view[1:,:num_post]
